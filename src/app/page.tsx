13b5226423d96bf44081e99fa5391fdd3191817c
Irfan <uiuxirfan@gmail.com>
2025-10-01 10:30:38 +0000

    Tambahkan button floating full rounded di pojok kanan atas untuk ganti t

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import FloatingMenu from '@/components/floating-menu';
+import Header from '@/components/layout/header';
 import PdfSummarizer from '@/components/style-analyzer';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-muted/40 p-4">
-      <PdfSummarizer />
-      <FloatingMenu />
-    </div>
+    <>
+      <Header />
+      <div className="flex min-h-screen w-full flex-col items-center justify-center bg-muted/40 p-4">
+        <PdfSummarizer />
+        <FloatingMenu />
+      </div>
+    </>
   );
 }
