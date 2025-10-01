2492051cce995505fa897aad0632fadbe52c6776
Irfan <uiuxirfan@gmail.com>
2025-10-01 03:32:37 +0000

    Ganti seluruh font dengan overpass font https://fonts.google.com/specime

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['Poppins', 'sans-serif'],
+        body: ['Overpass', 'sans-serif'],
+        headline: ['Overpass', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
