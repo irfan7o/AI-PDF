b3ec8c69ebdb6ed789c478af4878310737b79d47
Irfan <uiuxirfan@gmail.com>
2025-10-01 10:56:56 +0000

    Buat ukuran logo sama dengan ukuran light/dark mode

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,12 +1,12 @@
 export default function Logo() {
   return (
     <svg
-      width="32"
-      height="32"
+      width="40"
+      height="40"
       viewBox="0 0 32 32"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className="h-8 w-8"
+      className="h-10 w-10"
     >
       <path
         d="M16 4C9.37258 4 4 9.37258 4 16C4 22.6274 9.37258 28 16 28C22.6274 28 28 22.6274 28 16C28 9.37258 22.6274 4 16 4Z"
